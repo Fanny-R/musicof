75f40d8923fa9de7a144f97f54080a2fd6532ff2
Fanny Rottee <frottee@tea-ebook.com>
2018-09-10 21:53:07 +0200

    Put the slice-manipulation func in a separate file

--- slack/handler.go
+++ slack/handler.go
@@ -136,26 +136,3 @@ func (r *rtmBot) handleNominate(callerID string) error {
 
 	return err
 }
-
-func filter(in []string, excludedValues ...string) []string {
-	var res []string
-	for _, value := range in {
-		if find(value, excludedValues) {
-			continue
-		}
-
-		res = append(res, value)
-	}
-
-	return res
-}
-
-func find(n string, h []string) bool {
-	for _, v := range h {
-		if v == n {
-			return true
-		}
-	}
-
-	return false
-}
