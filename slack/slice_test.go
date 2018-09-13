ef9c122bf50ae38646037690bf44e2cf60a77f01
Fanny Rottee <frottee@tea-ebook.com>
2018-09-13 13:58:08 +0200

    Use test tables in tests

--- slack/slice_test.go
+++ slack/slice_test.go
@@ -6,31 +6,82 @@ import (
 )
 
 func TestFindReturnsTrueWhenSliceContainsGivenString(t *testing.T) {
-	n := "Caninos"
-	h := []string{"Simularbre", "Caninos", "Noctali", "Gobou"}
+	cases := []struct {
+		n string
+		h []string
+	}{
+		{"Caninos", []string{"Simularbre", "Caninos", "Noctali", "Gobou"}},
+		{"Caninos", []string{"Caninos"}},
+	}
 
-	if find(n, h) != true {
-		t.Errorf("Expected true, got false when finding %s in %s", n, h)
+	for _, testCase := range cases {
+		if find(testCase.n, testCase.h) != true {
+			t.Errorf("Expected true, got false when finding %s in %s", testCase.n, testCase.h)
+		}
 	}
 }
 
 func TestFindReturnsFalseWhenSliceDoesNotContainGivenString(t *testing.T) {
-	n := "Balignon"
-	h := []string{"Simularbre", "Caninos", "Noctali", "Gobou"}
+	cases := []struct {
+		n string
+		h []string
+	}{
+		{"Balignon", []string{"Simularbre", "Caninos", "Noctali", "Gobou"}},
+		{"Balignon", []string{}},
+		{"Balignon", []string{"Caninos"}},
+		{"", []string{"Simularbre", "Caninos", "Noctali", "Gobou"}},
+	}
 
-	if find(n, h) != false {
-		t.Errorf("Expected false, got true when finding %s in %s", n, h)
+	for _, testCase := range cases {
+		if find(testCase.n, testCase.h) != false {
+			t.Errorf("Expected false, got true when finding %s in %s", testCase.n, testCase.h)
+		}
 	}
 }
 
 func TestFilterReturnsANewSliceWithoutTheExcludedVAlues(t *testing.T) {
-	in := []string{"Simularbre", "Caninos", "Noctali", "Gobou"}
-	excludedValues := "Caninos"
+	cases := []struct {
+		in             []string
+		excludedValues []string
+		expectedResult []string
+	}{
+		{
+			[]string{"Simularbre", "Caninos", "Noctali", "Gobou"},
+			[]string{"Caninos"},
+			[]string{"Simularbre", "Noctali", "Gobou"},
+		},
+		{
+			[]string{"Caninos"},
+			[]string{"Caninos"},
+			[]string{},
+		},
+		{
+			[]string{"Simularbre", "Noctali", "Caninos", "Gobou"},
+			[]string{"Caninos", "Noctali"},
+			[]string{"Simularbre", "Gobou"},
+		},
+		{
+			[]string{"Simularbre", "Caninos", "Noctali", "Gobou"},
+			[]string{"Caninos", "Évoli"},
+			[]string{"Simularbre", "Noctali", "Gobou"},
+		},
+		{
+			[]string{"Simularbre", "Noctali", "Gobou"},
+			[]string{"Caninos"},
+			[]string{"Simularbre", "Noctali", "Gobou"},
+		},
+		{
+			[]string{"Simularbre", "Noctali", "Gobou"},
+			[]string{},
+			[]string{"Simularbre", "Noctali", "Gobou"},
+		},
+	}
 
-	expectedResult := []string{"Simularbre", "Noctali", "Gobou"}
-	result := filter(in, excludedValues)
+	for _, testCase := range cases {
+		result := filter(testCase.in, testCase.excludedValues...)
 
-	if reflect.DeepEqual(result, expectedResult) != true {
-		t.Errorf("Expected %s, got %s", expectedResult, result)
+		if reflect.DeepEqual(result, testCase.expectedResult) != true {
+			t.Errorf("Expected %s, got %s", testCase.expectedResult, result)
+		}
 	}
 }
